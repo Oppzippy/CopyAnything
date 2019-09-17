e6a31c67d1243ddbce4895c109f133b7cff73852
Kyle Scheuing <oppzippy@gmail.com>
2019-09-17 05:09:52 -0400

    Performance improvements

--- Core.lua
+++ Core.lua
@@ -1,18 +1,17 @@
 _G['SLASH_COPY1'] = '/copyanything'
 _G['SLASH_COPY2'] = '/copy'
 
-local EnumerateFrames = EnumerateFrames
+local EnumerateFrames, MouseIsOver = EnumerateFrames, MouseIsOver
 
 local function GetFontStrings(condition)
     local fontStrings = {}
     local frame = EnumerateFrames()
     while frame do
-        if frame:GetNumRegions() >= 1 then
-            local regions = { frame:GetRegions() }
-            for i, region in next, regions do
-                if region:GetObjectType() == "FontString" and condition(region) then
-                    fontStrings[#fontStrings+1] = region
-                end
+        local regions = { frame:GetRegions() }
+        for i, region in next, regions do
+            -- much faster to check if GetText than to use GetObjectType and check if FontString
+            if region.GetText and condition(region) then
+                fontStrings[#fontStrings+1] = region
             end
         end
         frame = EnumerateFrames(frame)
@@ -20,15 +19,17 @@ local function GetFontStrings(condition)
     return fontStrings
 end
 
-SlashCmdList['COPY'] = function()
-    local t = debugprofilestop()
-    local frames = GetFontStrings(function(f)
-        return MouseIsOver(f) and f:IsVisible()
-    end)
-    print(debugprofilestop()-t)
+local function FilterFontString(fontString)
+    return fontString:IsVisible() and MouseIsOver(fontString)
+end
+
+local function CopyText()
+    local frames = GetFontStrings(FilterFontString)
     local texts = {}
     for i, f in ipairs(frames) do
         texts[i] = f:GetText()
     end
     LibStub('LibCopyPaste-1.0'):Copy('Copy Anything', table.concat(texts, '\n'))
 end
+
+SlashCmdList['COPY'] = CopyText
