80c0762011ac71a44803cc581d55b6e8ff33876e
Kyle Scheuing <oppzippy@gmail.com>
2019-09-17 09:07:37 -0400

    Improve esES translations

--- Locales/esES.lua
+++ Locales/esES.lua
@@ -1,4 +1,4 @@
 if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then return end
 
-_G["BINDING_HEADER_COPYANYTHING"] = "Copiar Cualquier Cosa"
-_G["BINDING_NAME_COPYANYTHING_SHOW"] = "Mostrar"
+_G["BINDING_HEADER_COPYANYTHING"] = "Copia Cualquier Cosa"
+_G["BINDING_NAME_COPYANYTHING_SHOW"] = "Mostra"
