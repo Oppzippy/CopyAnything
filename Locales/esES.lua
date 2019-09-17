3b9fc76a239aa586185e67e8d04ee4c56467bf1d
Kyle Scheuing <oppzippy@gmail.com>
2019-09-17 05:46:42 -0400

    Add esES translations

--- Locales/esES.lua
+++ Locales/esES.lua
@@ -1,4 +1,4 @@
 if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then return end
 
--- _G["BINDING_HEADER_COPYANYTHING"] = "Copy Anything"
--- _G["BINDING_NAME_COPYANYTHING_SHOW"] = "Show"
+_G["BINDING_HEADER_COPYANYTHING"] = "Copiar Cualquier Cosa"
+_G["BINDING_NAME_COPYANYTHING_SHOW"] = "Mostrar"
