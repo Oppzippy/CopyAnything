bc7c2507abf18d7577684af8d561ced10c57584f
Kyle Scheuing <oppzippy@gmail.com>
2019-10-14 03:19:30 -0400

    Use packager to pull curseforge locales

--- Locales/zhTW.lua
+++ Locales/zhTW.lua
@@ -1,26 +1,4 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("CopyAnything", "zhTW")
 if not L then return end
 
--- General
--- L.copyAnything = "Copy Anything"
-
--- Bindings
--- L.show = "Show"
-
--- Options
--- L.general = "General"
--- L.profiles = "Profiles"
--- L.settings = "Settings"
-
--- Search types
--- L.searchType = "Search Type"
--- L.searchTypeDesc = "Method to use for searching for frames under the cursor."
--- L.searchTypeDescExtended = "Font Strings (default) - Search for individual FontStrings under the cursor\nParent Frames - Search for top level frames under the cursor, and copy all text from their children.\nMouse Focus - Copy text from the mouse focus frame. Only works on frames that are registered for mouse events."
--- L.fontStrings = "Font Strings"
--- L.parentFrames = "Parent Frames"
--- L.mouseFocus = "Mouse Focus"
-
--- Errors
--- L.invalidSearchType = "Invalid search type '%s'. Check options."
--- L.noTextFound = "No text found."
--- L.tooManyFontStrings = "More than %d font strings were found. The copy was cancelled to prevent the game from freezing for an excessive amount of time."
+--@localization(locale="zhTW", handle-unlocalized="comment")
