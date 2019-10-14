291d81a0dbd0fc1e2cf246c4dbe3ef5925d9cc8e
Kyle Scheuing <oppzippy@gmail.com>
2019-10-14 03:27:28 -0400

    Use multiline string

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -16,7 +16,9 @@ L.profiles = "Profiles"
 -- Search types
 L.searchType = "Search Type"
 L.searchTypeDesc = "Method to use for searching for frames under the cursor."
-L.searchTypeDescExtended = "Font Strings (default) - Search for individual FontStrings under the cursor.\nParent Frames - Search for top level frames under the cursor, and copy all text from their children.\nMouse Focus - Copy text from the mouse focus frame. Only works on frames that are registered for mouse events."
+L.searchTypeDescExtended = [=[Font Strings (default) - Search for individual FontStrings under the cursor.
+Parent Frames - Search for top level frames under the cursor, and copy all text from their children.
+Mouse Focus - Copy text from the mouse focus frame. Only works on frames that are registered for mouse events.]=]
 L.fontStrings = "Font Strings"
 L.parentFrames = "Parent Frames"
 L.mouseFocus = "Mouse Focus"
